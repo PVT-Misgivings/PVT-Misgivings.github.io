ce7ae05065a08fdbd39450543f8388b983225aed
PVT-Misgivings <cedjhat@gmail.com>
2024-02-06 19:22:42 +0100

    Adding in some phaser

--- math/script.js
+++ math/script.js
@@ -1,3 +1,4 @@
+let phaserScene;
 function findCosine(vLength, angle) {
     return vLength*Math.cos(angle*2*Math.PI/360);
 }
@@ -25,6 +26,55 @@ function calculate() {
     let finalYValue=addComponents(v1Sine,v2Sine);
 
     document.getElementById('answer').innerText =`${finalXValue.toFixed(2)}R + ${finalYValue.toFixed(2)}U`;
+
+    let l1 = phaserScene.drawLine(0, 0, v1Cosine, v1Sine, 0x0000FF);
+    let l2 = phaserScene.drawLine(0, 0, v2Cosine, v2Sine, 0xFF0000);
+    phaserScene.moveTo(phaserScene.drawLine(0, 0, v1Cosine, v1Sine, 0xFFFFFF), v2Cosine, v2Sine, finalXValue, finalYValue);
 }
 
+class Example extends Phaser.Scene {
+    sf = 10;
+    constructor() { 
+        super();
+        phaserScene = this;
+    }
 
+    preload () {
+        this.cameras.main.setZoom(.5);
+        this.cameras.main.pan(0, 0, 0);
+        this.add.line(0, 0, -100*this.sf, 0*this.sf, 100*this.sf, 0*this.sf, 0x000000, .5).setOrigin(0);
+        this.add.line(0, 0, 0*this.sf, -100*this.sf, 0*this.sf, 100*this.sf, 0x000000, .5).setOrigin(0);
+    }
+
+    create () {
+    }
+
+    drawLine(x1, y1, x2, y2, color) {
+        return this.add.line(0, 0, x1*this.sf, -y1*this.sf, x2*this.sf, -y2*this.sf, color, 0.5).setOrigin(0);
+    }
+
+    moveTo(line1, x, y, xf, yf) {
+        this.tweens.add({
+            targets: line1,
+            x: this.sf*x,
+            y: this.sf*-y,
+            ease: 'Linear',
+            repeat: 0,
+            duration: 2000,
+            onComplete: _ => {
+                this.add.line(0, 0, 0, 0, xf*this.sf, -yf*this.sf, 0xFFFFFF).setOrigin(0)
+            }
+        });
+
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: Example,
+    backgroundColor: 0x4488aa,
+};
+
+const game = new Phaser.Game(config);
