fd5ca12396d576996125dcab57d170b012301b5f
PVT-Misgivings <cedjhat@gmail.com>
2025-02-19 20:29:17 +0100

    Fixed bugs in equation

--- math/pointToLineCalculator.js
+++ math/pointToLineCalculator.js
@@ -1,15 +1,16 @@
 function calculate() {
-    let pointX=document.getElementById('pointX').value;
-    let pointY=document.getElementById('pointY').value;
-    let magnitude=document.getElementById('Magnitude').value;
-    let yIntercept=document.getElementById('Y-intercept').value;
-    let negNewMag = 1/magnitude;
-    let newB = pointY + (negNewMag*pointX);
-    let newX = (newB + yIntercept) / (magnitude + negNewMag);
+    let pointX=parseFloat(eval(document.getElementById('pointX').value));
+    let pointY=parseFloat(eval(document.getElementById('pointY').value));
+    let magnitude=parseFloat(eval(document.getElementById('Magnitude').value));
+    let yIntercept=parseFloat(eval(document.getElementById('YIntercept').value));
+    let newMag = -1/magnitude;
+    let newB = pointY - (newMag*pointX);
+    let newX = (newB - yIntercept) / (magnitude - newMag);
     let newY = magnitude*newX + yIntercept;
 
     let answer = Math.sqrt(((pointX-newX)**2) + ((pointY-newY)**2));
     document.getElementById('answer').innerHTML =answer;
 
 }
+
 
